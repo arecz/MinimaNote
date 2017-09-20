8af3c5ad890dbcbc93968d584b8f9c9ab87f42a0
Arkadiusz <nedar3@gmail.com>
2017-09-20 19:32:19 +0200

    add index.js, changed gulpfile for sass, add sass folder and sass file

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,15 @@
-require('./gulp/tasks/styles');
-require('./gulp/tasks/watch');
+var gulp = require("gulp");
+var watch = require("gulp-watch");
+var sass = require("gulp-sass");
 
+gulp.task("sass", function (){
+   return gulp.src("./sass/*.scss")
+    .pipe(sass())
+    .pipe(gulp.dest("./styles"))
+});
 
-
-
-
+gulp.task("watch", function(){
+   watch ("./sass/*scss", function (){
+      gulp.start("sass") 
+   });
+});
