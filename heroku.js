bf7def09391178e5b7e9174c98b85978c35f3d04
Arkadiusz <nedar3@gmail.com>
2017-09-21 20:46:01 +0200

    add heroku.js, change start script for heroku

---
+++ heroku.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+
+app.use(express.static(__dirname + '/public'));
+
+app.set ("view engine", "ejs");
+
+app.get("/", function(req, res){
+  res.render("landing")
+}); 
+
+app.get("/minimanote", function(req, res){
+  res.render("index") 
+});
+
+
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Heroku");
+})
