45ec50f0a2dad390e116944024304c78cad42283
Arkadiusz <nedar3@gmail.com>
2017-09-27 21:20:26 +0200

    add animations in functions section

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,9 @@
 var hamburger       = document.querySelector(".navbar-content-mobile"),
     hamburgerList   = document.querySelector(".mobile-list"),
-    arrow           = document.querySelector(".header-footer--logo")
+    arrow           = document.querySelector(".header-footer--logo"),
+    column          = document.querySelector("#plus"),
+    plus            = document.querySelector(".fa-plus-circle"),
+    plusText        = document.querySelector(".plus-text");
 
 hamburger.addEventListener("click", function(){
     hamburgerList.classList.toggle("navbar-visible");
@@ -9,11 +12,18 @@ hamburger.addEventListener("click", function(){
 var timer
 
 arrow.addEventListener("click", function(){
-  timer = setInterval(function(){scrollBy(0, 6)                              
+  timer = setInterval(function(){scrollBy(0, 20)                              
     if (window.scrollY > 780) {
       clearInterval(timer);
     }
   }, 1);
 });
 
+
+
+column.addEventListener("click", function(){
+  plus.classList.toggle("invisible");
+  plusText.classList.toggle("fade-in");
+  column.classList.toggle("background-fade-in");
+});
                       
